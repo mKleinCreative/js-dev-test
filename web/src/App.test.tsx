feb71f862c7e9cec3904c93fd6b35b7e4e310365
Brandon Goldsworthy <brandon.goldsworthy@gmail.com>
2021-12-29 07:49:34 -0800

    update dependencies, refresh CRA default app

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,10 +1,9 @@
-/* eslint-env jest */
-
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
