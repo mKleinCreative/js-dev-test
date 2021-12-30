78155c7bfa3a58cf15a58f38947341d9e08b61ce
Michael <miketheklein@gmail.com>
2021-12-30 12:29:56 -0600

    Base api is able to make calls to external data, need to fix TS issues for local data

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import axios from 'axios'
+import { readFile } from 'fs'
 
 export const repos = express.Router();
 
-repos.get('/', (req, res) => {
+interface repoJson {
+  fork: boolean;
+}
+
+repos.get('/', async (req, res) => {
   res.header('Content-Type', 'application/json');
+  let local = readFile('../data/repos.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    JSON.parse(data)
+  });
+  let dataResponse = await axios.get('https://api.github.com/users/silverorange/repos')
+  dataResponse = dataResponse.data.filter((repo: repoJson) => {
+    return repo.fork === false
+  })
+
+  console.log(local)
   res.status(200);
-  res.send('[]');
+  res.send({dataResponse});
 });
