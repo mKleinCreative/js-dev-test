c99b07c29d21bc101b3516178fc2d38342f239bb
Michael <miketheklein@gmail.com>
2021-12-30 15:26:10 -0600

    solved local problem, moving to client work

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,25 +1,35 @@
 import express from 'express';
-import axios from 'axios'
-import { readFile } from 'fs'
+import axios from 'axios';
+import { readFile } from 'fs';
+import util from 'util';
 
 export const repos = express.Router();
 
-interface repoJson {
+interface RepoJson {
   fork: boolean;
 }
 
+//Allowing readFile to be used a promise
+const open = util.promisify(readFile);
+
+// function to get the data from the local data JSON file
+const getLocal = open('../data/repos.json', 'utf8').then((data) => {
+  const returned = JSON.parse(data);
+  return returned.filter((repo: RepoJson) => repo.fork === false);
+});
+
 repos.get('/', async (req, res) => {
   res.header('Content-Type', 'application/json');
-  let local = readFile('../data/repos.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    JSON.parse(data)
-  });
-  let dataResponse = await axios.get('https://api.github.com/users/silverorange/repos')
-  dataResponse = dataResponse.data.filter((repo: repoJson) => {
-    return repo.fork === false
-  })
+  const local = await getLocal;
 
-  console.log(local)
+  let dataResponse = await axios.get(
+    'https://api.github.com/users/silverorange/repos'
+  );
+
+  dataResponse = dataResponse.data.filter((repo: RepoJson) => {
+    return repo.fork === false;
+  });
+  const allRepos = { ...dataResponse, local };
   res.status(200);
-  res.send({dataResponse});
+  res.send({ allRepos });
 });
